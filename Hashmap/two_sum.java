ced6ea52c8b86b84a17c478d3dd48e3cc335e2b7
sanjayvanan <sanjayvanan03@gmail.com>
2024-04-24 00:04:11 +0530

    two sum

---
+++ Hashmap/two_sum.java
@@ -0,0 +1,22 @@
+package Hashmap;
+import java.util.*;
+class two_sum{
+    public static void main(String[] args){
+        int[] arr = {11,3,7,9,14,2};
+        int target = 16;
+        int[] ans = new int[2];
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i=0;i<arr.length;i++){
+            int sec = target - arr[i];
+            if(map.containsKey(sec)){
+                ans[0] = map.get(sec);
+                ans[1] = i;
+                break;
+            }
+            map.put(arr[i],i);
+        }
+        System.out.println(ans[0]+" And "+ ans[1]);
+    }
+
+}
