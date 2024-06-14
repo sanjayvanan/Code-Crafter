d02d2d1ff8db6866462e4c7a804a4de44a43ea20
Sanjayvanan S G <102749975+sanjayvanan@users.noreply.github.com>
2024-06-14 23:58:22 +0530

    Update two_sum.java

--- Hashmap/two_sum.java
+++ Hashmap/two_sum.java
@@ -6,7 +6,6 @@ class two_sum{
         int target = 16;
         int[] ans = new int[2];
         HashMap<Integer, Integer> map = new HashMap<>();
-
         for(int i=0;i<arr.length;i++){
             int sec = target - arr[i];
             if(map.containsKey(sec)){
